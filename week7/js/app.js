aad003a1a096938720691482b6e0b687956e609c
takanbi <tunji.akanbi@century.edu>
2024-10-09 12:53:01 -0500

    tested some gsap animation

--- week7/js/app.js
+++ week7/js/app.js
@@ -1,22 +1,30 @@
-$(document).ready(function(){
-  document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger,ScrollToPlugin)
-let menu = $("#menu");
-let mobileNav = $("#mobile-nav");
+$(function () {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+  let menu = $("#menu");
+  let mobileNav = $("#mobile-nav");
+  let mobileLinks = $("#mobile-nav-ul > li");
+  console.log(mobileLinks);
   mobileNav.addClass("hide");
   let isShow = false;
-  menu.click(()=> {
+  menu.on("click",() => {
     showNav();
   });
   function showNav() {
-    if(isShow == false) {
-        mobileNav.addClass("show").removeClass("hide");
-        isShow = true;
-    }
-    else if(isShow == true) {
-        mobileNav.addClass("hide").removeClass("show");
-        isShow = false;
+    if (isShow == false) {
+      mobileNav.addClass("show").removeClass("hide");
+      isShow = true;
+      gsap.from(mobileNav, {autoAlpha: 0, duration: 0.5})
+      gsap.from(mobileLinks, {y: 300, autoAlpha: 0, stagger: 0.25}, "+=0.5");
+    } else if (isShow == true) {
+      // gsap.to(mobileNav, {autoAlpha: 1, duration: 0.5})
+      // gsap.to(mobileLinks, {y: 0, autoAlpha: 0, stagger: 0.25}, "+=0.5");
+      mobileNav.addClass("hide").removeClass("show");
+// setInterval(myTimer, 5000);
+// function myTimer() {
+//   mobileNav.addClass("hide").removeClass("show");
+// }
+      isShow = false;
     }
   }
+  //rework hamburger menu logic/////////////////////////////////
 });
-  });
